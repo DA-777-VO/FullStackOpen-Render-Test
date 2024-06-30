5bfeb1c7fdd9c6c47369bc9e59d93cab64440886
David Apakin <apakin.david@gmail.com>
2024-06-30 16:51:58 +0400

    Updated some fuaters. Added MongoDB

--- index.js
+++ index.js
@@ -9,46 +9,22 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
 
-
-
-// const mongoose = require('mongoose')
-//
-// const url = process.env.MONGODB_URL;
-//
-// mongoose.set('strictQuery',false)
-// mongoose.connect(url)
-//
-// const noteSchema = new mongoose.Schema({
-//     content: String,
-//     important: Boolean,
-// })
-//
-// noteSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//         returnedObject.id = returnedObject._id.toString()
-//         delete returnedObject._id
-//         delete returnedObject.__v
-//     }
-// })
-//
-// const Note = mongoose.model('Note', noteSchema)
-
 let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
+    // {
+    //     id: 1,
+    //     content: "HTML is easy",
+    //     important: true
+    // },
+    // {
+    //     id: 2,
+    //     content: "Browser can execute only JavaScript",
+    //     important: false
+    // },
+    // {
+    //     id: 3,
+    //     content: "GET and POST are the most important methods of HTTP protocol",
+    //     important: true
+    // }
 ]
 
 const requestLogger = (request, response, next) => {
@@ -65,6 +41,16 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -75,55 +61,66 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-const generateId = () =>{
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-    return maxId + 1
-}
-
-
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if(!body.content){
+    if(body.content === undefined){
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
+        important: body.important || false,
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 
-
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note){
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note){
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
