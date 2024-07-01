7c9639c9ef6259025561592a94cc4315df4ee1c3
David Apakin <apakin.david@gmail.com>
2024-07-01 22:55:06 +0400

    Updated some fuaters. Added moving of error handling to middleware. Added specific validation rules.

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError'){
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
@@ -55,13 +57,15 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+app.get('/api/notes', (request, response, next) => {
+    Note.find({})
+        .then(notes => {
+            response.json(notes)
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if(body.content === undefined){
@@ -75,9 +79,11 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 
@@ -94,7 +100,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
@@ -104,14 +110,13 @@ app.delete('/api/notes/:id', (request, response) => {
 
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const {content, important} = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+        {content, important},
+        { new: true, runValidators: true, context: 'query'}
+        )
         .then(updatedNote => {
             response.json(updatedNote)
         })
